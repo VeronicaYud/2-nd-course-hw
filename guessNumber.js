8fde118f243f9615795bb8554333f7b31e77e7cb
VeronicaYud <Yudina.veronika@gmail.com>
2025-01-20 17:07:35 +0500

    Готово

---
+++ guessNumber.js
@@ -0,0 +1,24 @@
+function getRandomInt(min, max)  {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function guessNumberGame() {
+    let number = getRandomInt(1, 100);
+    let userNumber;
+
+    do {
+        userNumber = prompt("Я загадала число. Угадай его!", "Вdеди число");
+        if (number > userNumber) {
+            alert("Ваше число меньше, чем загаданное");
+        } else if (number < userNumber) {
+            alert("Ваше число больше, чем загаданное");
+        } else {
+            alert("Вы угадали!");
+        }
+    } while(number != userNumber);
+
+    alert("Конец игры");
+}
+
